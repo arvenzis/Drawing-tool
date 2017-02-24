b8844a81dc39838b98ed477d612664ccff20db3b
Karen Bosch-Brakband <kbosch@dunique.nl>
2017-02-24 20:29:31 +0100

    Added the possibility to change the pencil color

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 (function($){
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-
-    //Detect a mouse down. Set the xy coordinates
     var mouseDown = false;
 
+    context.strokeStyle = '#000';
+
+    //Detect a mouse down. Set the xy coordinates
     $(canvas).mousedown(function(e){
         mouseDown = true;
 
         context.beginPath();
-        context.moveTo(e.pageX, e.pageY);
+        context.moveTo(e.offsetX, e.offsetY);
     });
 
     //Detect that the mouse is moving and draw the line while the mouse is still down
     $(canvas).mousemove(function(e){
         if(mouseDown){
-            var x = e.offsetX / 1.325325;
-            var y = e.offsetY / 2.65;
-
+            var x = e.offsetX;
+            var y = e.offsetY;
 
             context.lineTo(x, y);
-            context.strokeStyle = '#000';
             context.stroke();
 
         }
     });
 
-    //On mouse up, reset the coordinates
+    //On mouse up
     $(canvas).mouseup(function(){
+        stopDrawing();
+    });
+
+    //If the mouse leaves
+    $(canvas).mouseleave(function(){
+       stopDrawing();
+    });
+
+    function stopDrawing(){
         mouseDown = false;
         context.closePath();
+    }
+
+    $('.color-container').click(function(){
+        context.strokeStyle = this.id;
     });
+
+
 
 })(jQuery);
